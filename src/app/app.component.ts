d415c1a2d39ead94a193249dd537558e2564c48a
dangvanthua <thuandang021102@gmail.com>
2024-11-13 12:23:57 +0700

    create user  inteface home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { ProductComponent } from './pages/product/product.component';
 import { OrderComponent } from './pages/order/order.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RegisterComponent],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
